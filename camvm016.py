e1aea720a25dbe9491d09942022c49889b8b3e17
Stefan Riedmueller <s.riedmueller@phytec.de>
2022-10-24 15:09:37 +0200

    camvm016: Fix camera setup
    With our hardknott BSP release the V4L2 controls from the sensor are no
    longer forwarded to the video device, thus set them on the corresponding
    subdevice.
    
    Also using the video device now requires to explicitly set the selected
    format when starting a gstreamer pipeline.
    
    While at it, make use of the new setup-pipeline scripts.
    
    Signed-off-by: Stefan Riedmueller <s.riedmueller@phytec.de>

--- camvm016.py
+++ camvm016.py
@@ -26,66 +26,30 @@ def get_camera():
     if not os.path.exists(path):
         return None
 
-    failed = False
-    # Make sure the camera is in a defined state
-    cmd = 'media-ctl -V "31:0[fmt:SGRBG8_1X8/1280x800 (4,4)/1280x800]"'
-    ret = subprocess.call(cmd, shell=True)
-    if ret != 0:
-        print('media-ctl failed: {}'.format(ret))
-        failed = True
-    cmd = 'media-ctl -V "22:0[fmt:SGRBG8_1X8/1280x800]"'
-    ret = subprocess.call(cmd, shell=True)
-    if ret != 0:
-        print('media-ctl failed: {}'.format(ret))
-        failed = True
-    cmd = 'v4l2-ctl -d0 -v width=1280,height=800,pixelformat=GRBG'
-    ret = subprocess.call(cmd, shell=True)
-    if ret != 0:
-        print('v4l2-ctl failed: {}'.format(ret))
-        failed = True
-    cmd = 'v4l2-ctl -d0 -c vertical_flip=1'
-    ret = subprocess.call(cmd, shell=True)
-    if ret != 0:
-        print('v4l2-ctl failed: {}'.format(ret))
-        failed = True
-    cmd = 'v4l2-ctl -c horizontal_blanking=2500'
-    ret = subprocess.call(cmd, shell=True)
-    if ret != 0:
-        print('v4l2-ctl failed: {}'.format(ret))
-        failed = True
-    cmd = 'v4l2-ctl -c digital_gain_red=1400'
-    ret = subprocess.call(cmd, shell=True)
-    if ret != 0:
-        print('v4l2-ctl failed: {}'.format(ret))
-        failed = True
-    cmd = 'v4l2-ctl -c digital_gain_blue=1700'
-    ret = subprocess.call(cmd, shell=True)
-    if ret != 0:
-        print('v4l2-ctl failed: {}'.format(ret))
-        failed = True
+    width = 1280
+    height = 800
 
-    if failed:
-        vd = os.open('/dev/video0', os.O_RDWR | os.O_NONBLOCK, 0)
-        btype = ctypes.c_uint(V4L2_BUF_TYPE_VIDEO_CAPTURE_MPLANE)
-        fcntl.ioctl(vd, VIDIOC_STREAMOFF, btype)
-        os.close(vd)
+    size = f'{width}x{height}'
+    cmd = f'setup-pipeline-csi1 -s {size} -c {size}'
+    ret = subprocess.call(cmd, shell=True)
+    if ret != 0:
+        subprocess.call(cmd, shell=True)
 
-        cmd = 'media-ctl -V "31:0[fmt:SGRBG8_1X8/1280x800 (4,4)/1280x800]"'
-        ret = subprocess.call(cmd, shell=True)
-        cmd = 'media-ctl -V "22:0[fmt:SGRBG8_1X8/1280x800]"'
-        ret = subprocess.call(cmd, shell=True)
-        cmd = 'v4l2-ctl -d0 -v width=1280,height=800,pixelformat=GRBG'
-        ret = subprocess.call(cmd, shell=True)
-        cmd = 'v4l2-ctl -d0 -c vertical_flip=1'
-        ret = subprocess.call(cmd, shell=True)
-        cmd = 'v4l2-ctl -c horizontal_blanking=2500'
-        ret = subprocess.call(cmd, shell=True)
-        cmd = 'v4l2-ctl -c digital_gain_red=1400'
-        ret = subprocess.call(cmd, shell=True)
-        cmd = 'v4l2-ctl -c digital_gain_blue=1700'
-        ret = subprocess.call(cmd, shell=True)
+    controls = [
+        '-c vertical_flip=1',
+        '-c horizontal_blanking=2500',
+        '-c digital_gain_red=1400',
+        '-c digital_gain_blue=1700',
+    ]
+    cmd = f'v4l2-ctl -d /dev/cam-csi1 {" ".join(controls)}'
+    print(cmd)
+    ret = subprocess.call(cmd, shell=True)
+    if ret != 0:
+        print('v4l2-ctl failed: {}'.format(ret))
+        subprocess.call(cmd, shell=True)
 
-    pipeline = 'v4l2src device=/dev/{video} ! appsink'.format(video=videodev)
+    fmt = f'video/x-bayer,format=grbg,width={width},height={height}'
+    pipeline = f'v4l2src device=/dev/video-csi1 ! {fmt} ! appsink'
     return cv2.VideoCapture(pipeline, cv2.CAP_GSTREAMER)
 
 
